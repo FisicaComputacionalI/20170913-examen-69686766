bfc49048bdd980f98f2a489688e726fcd16ed4e1
69686766 <31351800+69686766@users.noreply.github.com>
2017-09-13 14:04:28 -0500

    Add files via upload
    llegue hasta aquí en la hora que nos dijo lo terminare de todas formas

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import numpy as np 
 import matplotlib.pyplot as plt 
-# definimos las variable en el rango en el rango en que cambian 
+import numpy as np 
+#definimos las variables y el rango en que corren
+x=np.arange(0.0, 21, 1)
+y=np.arange(1096, 2017, 1)
+
 t1=np.arange(0.0, 5, 0.2 )
 t2=np.cos(2*np.pi*t1)*4
-#graficamos la variable t1 contra la variable t2 con linea punteada de color negro
-plt.plot(t1,t2,'k--')
-#guardamos la figura que creamos 
-plt.savefig('grafica2.png')
-#muestra la grafica
-plt.show()
-
+#crea el grupo de graficas
+plt.figure(1)
+#crea en el lienzo con dos renglones, una columna y entra en la primera seccion 
+plt.subplot(211)
+#grafica la variable x contra 
