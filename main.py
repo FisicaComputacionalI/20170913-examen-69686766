cdb8104f6f62f3bda12786a6250d23a27b1e0277
69686766 <31351800+69686766@users.noreply.github.com>
2017-09-13 13:27:51 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ pl.title('crescenciano cuautle coyotl')
 # crea un vector con los valores del eje x
 x=(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21)
 # titulo del eje
-pl.xlabel('anios')
+pl.xlabel('Edad')
 y=(1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017)
-pl.ylabel('anio en que naci')
+pl.ylabel('anio')
 #grafica del vector x contra el vector y
 pl.plot(x,y)
 #guarda la imagen
