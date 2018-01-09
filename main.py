566753f6d706c5e747dc37492eb147cee337c279
Isabel Pedraza <isapedraza@hotmail.com>
2018-01-09 01:19:25 +0100

    calificacion de examen

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ x=(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21)
 y=(1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017)
 
 t1=np.arange(0.0,5,0.2)
+#Tu definici'on del coseno no es apropiada. Te falt'o aniadir como cosntante el anio en el que entraste a la universidad.
 t2=np.cos(2*np.pi*t1)*4
 #creamos e lgrupo de graficas 
 plt.figure(1)
